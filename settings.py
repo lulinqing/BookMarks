5eedc807cfa53db1646f62b0cb0df09e853e0958
Linqing Lu <lilu@redhat.com>
2012-02-29 19:27:40 +0800

    minor improvements

--- settings.py
+++ settings.py
@@ -104,7 +104,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'django_bookmarks.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -124,7 +124,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'django_bookmarks.bookmarks',
+    'bookmarks',
     'django.contrib.comments',
 )
 
